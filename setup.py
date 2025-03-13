56f47750c9a3ded9583d688c868eab3dcd49d32d
andreysantos01 <107321634+andreysantos01@users.noreply.github.com>
2025-03-13 19:47:07 -0300

    Primeiro pacote criado
    Primeiro pacote criado

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+setup(
+    name="image_processing",  
+    version="0.0.1",
+    author="Andrey Santos",
+    author_email="correaandrey25@gmail.com",
+    description="My short description",  
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andreysantos01?tab=repositories",  
+    packages=find_packages(),
+    install_requires=[],  
+    python_requires=">=3.8",
+)
